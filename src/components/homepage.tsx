1c006cb8288c2bed946523257098186ad350d875
EthanWakeford <ethanwakeford@gmail.com>
2023-11-04 16:46:56 -0500

    homepage

--- src/components/homepage.tsx
+++ src/components/homepage.tsx
@@ -1,7 +1,16 @@
 const Homepage = () => {
   return (
     <>
-      <div>this is a hojmepage</div>
+      <h2>Welcome to the events planning whatever thing</h2>
+      <p>this is where we give a short tagline</p>
+      <ul>
+        <li>short</li>
+        <li>list</li>
+        <li>of projects</li>
+      </ul>
+      <div>
+        perhapss some instructions on how to create your own event proposal
+      </div>
     </>
   );
 };
