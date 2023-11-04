7d0d699f508407f3259efa02efa12235e25c9239
EthanWakeford <ethanwakeford@gmail.com>
2023-11-04 17:38:47 -0500

    sexy header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,31 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Header = () => {
+  const [hamburgerEaten, setHamburgerEaten] = useState<boolean>(false);
+
+  const consume = () => {
+    setHamburgerEaten(!hamburgerEaten);
+  };
+
   return (
     <>
       <HeaderWrapper>
-        <Title>Hackython</Title>
+        <Link to={'/'}>
+          <Title>Hackython</Title>
+        </Link>
+        <Hamburger
+          onClick={consume}
+          className='fa-solid fa-bars'
+          $hamburgerEaten={hamburgerEaten}
+        />
+        <NavContainer $hamburgerEaten={hamburgerEaten}>
+          <Link to={'/'}>Home</Link>
+          <Link to={'/proposals'}>Proposals</Link>
+          <Link to={'/projects'}>Projects</Link>
+          <i className='fa-solid fa-xmark' onClick={consume} />
+        </NavContainer>
       </HeaderWrapper>
       <Spacer />
     </>
@@ -22,6 +43,25 @@ const HeaderWrapper = styled.div`
 const Title = styled.h1`
   text-align: center;
   margin: 0;
+`;
+
+const Hamburger = styled.i<{ $hamburgerEaten: boolean }>`
+  position: absolute;
+  top: 2vh;
+  right: 2vw;
+  font-size: xx-large;
+  ${(props) => props.$hamburgerEaten && 'display: none'}
+`;
+
+const NavContainer = styled.div<{ $hamburgerEaten: boolean }>`
+  position: absolute;
+  top: 2vh;
+  right: 2vw;
+  font-size: larger;
+  display: none;
+  gap: 10px;
+  align-items: center;
+  ${(props) => props.$hamburgerEaten && 'display: flex'}
 `;
 
 const Spacer = styled.div`
