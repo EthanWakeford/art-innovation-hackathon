6072876b64249b5337514eb83c5b70a835d28a66
EthanWakeford <ethanwakeford@gmail.com>
2023-11-04 17:21:37 -0500

    project card holder

--- src/components/homepage.tsx
+++ src/components/homepage.tsx
@@ -1,3 +1,4 @@
+import styled from 'styled-components';
 import ProjectCard from './ProjectCard';
 
 const Homepage = () => {
@@ -5,26 +6,47 @@ const Homepage = () => {
     <>
       <h2>Welcome to the events planning whatever thing</h2>
       <p>this is where we give a short tagline</p>
-      <ul>
-        <li>short</li>
-        <li>list</li>
-        <li>of projects</li>
-      </ul>
+      <ProjectsCardHolder>
+        <ProjectCard
+          project={{
+            id: '000001',
+            name: 'Fancy Party',
+            description: 'A party, but fancy.',
+            imageUrl:
+              'https://ci6.googleusercontent.com/proxy/7majMCZYAEq6aBZmv0LCwogQ63X961cgncekZVcEqXGUsugKkfNJqb-VWgUJ66jfCWT9J-p2_sAgh1aAoa92-OCGW4J6DYqQiClT0Xw=s0-d-e1-ft#https://avatars.githubusercontent.com/u/90289314?s=120&v=4',
+            eventDate: 'December 31, 2021',
+          }}
+        />
+        <ProjectCard
+          project={{
+            id: '000001',
+            name: 'Fancy Party',
+            description: 'A party, but fancy.',
+            imageUrl:
+              'https://ci6.googleusercontent.com/proxy/7majMCZYAEq6aBZmv0LCwogQ63X961cgncekZVcEqXGUsugKkfNJqb-VWgUJ66jfCWT9J-p2_sAgh1aAoa92-OCGW4J6DYqQiClT0Xw=s0-d-e1-ft#https://avatars.githubusercontent.com/u/90289314?s=120&v=4',
+            eventDate: 'December 31, 2021',
+          }}
+        />
+        <ProjectCard
+          project={{
+            id: '000001',
+            name: 'Fancy Party',
+            description: 'A party, but fancy.',
+            imageUrl:
+              'https://ci6.googleusercontent.com/proxy/7majMCZYAEq6aBZmv0LCwogQ63X961cgncekZVcEqXGUsugKkfNJqb-VWgUJ66jfCWT9J-p2_sAgh1aAoa92-OCGW4J6DYqQiClT0Xw=s0-d-e1-ft#https://avatars.githubusercontent.com/u/90289314?s=120&v=4',
+            eventDate: 'December 31, 2021',
+          }}
+        />
+      </ProjectsCardHolder>
       <div>
         perhapss some instructions on how to create your own event proposal
       </div>
-      <ProjectCard
-        project={{
-          id: '000001',
-          name: 'Fancy Party',
-          description: 'A party, but fancy.',
-          imageUrl:
-            'https://ci6.googleusercontent.com/proxy/7majMCZYAEq6aBZmv0LCwogQ63X961cgncekZVcEqXGUsugKkfNJqb-VWgUJ66jfCWT9J-p2_sAgh1aAoa92-OCGW4J6DYqQiClT0Xw=s0-d-e1-ft#https://avatars.githubusercontent.com/u/90289314?s=120&v=4',
-          eventDate: 'December 31, 2021',
-        }}
-      />
     </>
   );
 };
+
+const ProjectsCardHolder = styled.div`
+  display: flex;
+`;
 
 export default Homepage;
