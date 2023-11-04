deb554b7330855b6344cee5046f79535cdfa29de
EthanWakeford <ethanwakeford@gmail.com>
2023-11-04 16:55:34 -0500

    proposals component

--- src/components/proposals.tsx
+++ src/components/proposals.tsx
@@ -1,16 +1,20 @@
 import ProposalItem from './proposalItem';
 
-const proposalInfoHardcoded = [
+const proposalInfoHardcoded: Proposal[] = [
   {
     imageUrl: '',
     type: 'concert',
     name: 'roomates sisters jazz math rock',
-    data: 'never',
+    date: 'never',
     textInfo:
       'idk when its gonna happen but its gonna be greate lorem ipsum sit dolor amet whaterver',
   },
   {
     imageUrl: '',
+    type: 'art exhibition',
+    name: 'someones art exhibit',
+    date: 'idk',
+    textInfo: 'this sucks and no one wants to go to it',
   },
 ];
 
@@ -18,7 +22,18 @@ const Proposals: React.FC = () => {
   return (
     <>
       <h2>Open Proposals</h2>
-      <div>proposals go here</div>
+      <div>
+        {proposalInfoHardcoded.map((proposal) => (
+          <ProposalItem
+            open={false}
+            imageUrl={proposal.imageUrl}
+            type={proposal.type}
+            name={proposal.name}
+            date={proposal.date}
+            textInfo={proposal.textInfo}
+          />
+        ))}
+      </div>
     </>
   );
 };
