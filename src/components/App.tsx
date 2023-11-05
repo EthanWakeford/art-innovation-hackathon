3ed33a2629977b6ac7141859d97a4f196e89919a
EthanWakeford <ethanwakeford@gmail.com>
2023-11-05 10:43:43 -0600

    fixing itl

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,8 @@
 import { GlobalStyle } from '../GlobalStyle';
 import Header from './header';
 import { Outlet } from 'react-router-dom';
-import { MyWallet } from '../near/near-wallet';
 
 function App() {
-  console.log(new MyWallet());
-
   return (
     <>
       <GlobalStyle />
